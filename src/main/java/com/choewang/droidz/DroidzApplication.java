e2b5612c13c12ecc2178d8918749fcd0841cf53e
MorgothThe <tchoedol@gmail.com>
2019-12-22 13:35:39 +0100

    project initialization

---
+++ src/main/java/com/choewang/droidz/DroidzApplication.java
@@ -0,0 +1,13 @@
+package com.choewang.droidz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroidzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroidzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/choewang/droidz/DroidzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choewang.droidz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroidzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
